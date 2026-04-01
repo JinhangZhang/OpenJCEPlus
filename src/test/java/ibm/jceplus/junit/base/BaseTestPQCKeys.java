145bb91d0bb511c4f7817428531dac6d72f54cab
JinhangZhang <Jinhang.Zhang@ibm.com>
2026-04-01 00:54:16 -0400

    Tolerate RawKeySpec from Sun

--- src/test/java/ibm/jceplus/junit/base/BaseTestPQCKeys.java
+++ src/test/java/ibm/jceplus/junit/base/BaseTestPQCKeys.java
@@ -214,6 +214,22 @@ public class BaseTestPQCKeys extends BaseTestJunit5 {
         assertArrayEquals(newPubKey, pubKey, "Public key does not match generated public key - " + Algorithm);
         assertArrayEquals(newPrivKey, privKey, "Private key does not match generated public key - " + Algorithm);
 
+        assertArrayEquals(newPubKey, pubKey, "Public key does not match internal generated public key - " + Algorithm);
+        assertArrayEquals(newPrivKey, privKey, "Private key does not match internal generated private key - " + Algorithm);
+
+        sun.security.util.RawKeySpec pubSpecFromSun = new sun.security.util.RawKeySpec(pubKey);
+        sun.security.util.RawKeySpec privSpecFromSun = new sun.security.util.RawKeySpec(privKey);
+
+        PublicKey publicKeyFromSun = pqcKeyFactory.generatePublic(pubSpecFromSun);
+        PrivateKey privateKeyFromSun = pqcKeyFactory.generatePrivate(privSpecFromSun);
+
+        //Need to extract just the key material from the key to do the compare
+        byte[] newPubKeyFromSun = parsePub(publicKeyFromSun.getEncoded());
+        byte[] newPrivKeyFromSun = parsePriv(privateKeyFromSun.getEncoded());
+
+        assertArrayEquals(newPubKeyFromSun, pubKey, "Public key does not match Sun generated public key - " + Algorithm);
+        assertArrayEquals(newPrivKeyFromSun, privKey, "Private key does not match Sun generated private key - " + Algorithm);
+
     }
 
     byte[] parsePub(byte[] key) throws InvalidKeyException {
