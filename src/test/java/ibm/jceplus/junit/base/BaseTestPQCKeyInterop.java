2795deaf6cecf45943bccd538a2056e487daf23a
JinhangZhang <Jinhang.Zhang@ibm.com>
2026-06-05 10:04:16 -0400

    Add interop tests

--- src/test/java/ibm/jceplus/junit/base/BaseTestPQCKeyInterop.java
+++ src/test/java/ibm/jceplus/junit/base/BaseTestPQCKeyInterop.java
@@ -795,7 +795,7 @@ public class BaseTestPQCKeyInterop extends BaseTestJunit5Interop {
         KeyPairGenerator interopKpg = KeyPairGenerator.getInstance(algorithm, interopProviderName);
         KeyPair interopKeyPair = interopKpg.generateKeyPair();
         PrivateKey interopPrivateKey = interopKeyPair.getPrivate();
-        KeySpec interopPrivKeySpec = PKCS8EncodedKeySpec(interopPrivateKey.getEncoded());
+        KeySpec interopPrivKeySpec = new PKCS8EncodedKeySpec(interopPrivateKey.getEncoded());
         PrivateKey openjceplusPrivateKey = openjceplusKeyFactory.generatePrivate(interopPrivKeySpec);
         KeySpec keySpec = openjceplusKeyFactory.getKeySpec(openjceplusPrivateKey, interopPrivKeySpec.getClass());
         assertEquals(interopPrivKeySpec.getClass(), keySpec.getClass());
